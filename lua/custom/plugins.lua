4fead9a0129705c9846ba103d4168d1b94bdbfc2
Qimat Luo <qimatluo@westpharma.com>
2023-06-20 21:31:36 +0800

    Setup LSP base

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -4,6 +4,38 @@ local plugins = {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+      },
+      indent = {
+        enable = true,
+      },
+    },
+  },
 }
 
 return plugins
