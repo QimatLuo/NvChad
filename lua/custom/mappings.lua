d9d79f9df1e203112d84debf32e441e68ab22fc5
Qimat Luo <qimatluo@westpharma.com>
2023-07-01 19:40:25 +0800

    Setup christoomey/vim-tmux-navigator

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -2,6 +2,12 @@
 local M = {}
 
 M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+  },
 }
 
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,9 @@
 ---@type NvPluginSpec[]
 local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
 }
 
 return plugins
